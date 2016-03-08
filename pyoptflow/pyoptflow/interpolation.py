a31f9da42a3bccad6b9449f24c6792721f39fdbe
Seppo Pulkkinen <pulkkins@gmail.com>
2016-03-08 09:59:14 +0200

    New version of interpolation that uses forward and backward motion fields.

--- pyoptflow/pyoptflow/interpolation.py
+++ pyoptflow/pyoptflow/interpolation.py
@@ -1,12 +1,12 @@
-"""Methods for temporal interpolation between images by using a motion vector 
-field."""
+"""Methods for motion-based temporal interpolation between two input images."""
 
 from numpy import arange, dstack, meshgrid, reshape, size
 from scipy.ndimage.interpolation import map_coordinates
 
-def interpolate(I1, I2, V, n):
-  """Interpolate n frames between two images by using a motion vector field. 
-  The time range between the images is equally divided into n subintervals.
+def interpolate(I1, I2, VF, n, VB=None):
+  """Interpolate n frames between two images by using forward and backward 
+  motion fields computed from two images. The time range between the images is 
+  equally divided into n subintervals.
   
   Parameters
   ----------
@@ -14,10 +14,13 @@ def interpolate(I1, I2, V, n):
     The first input image.
   I2 : array-like
     The second input image (must have the same shape as I1).
-  V : array-like
-    The motion vector field (must have the same shape as I1 and I2).
+  VF : array-like
+    The forward motion field (must have the same shape as I1 and I2).
   n : int
     Number of frames to interpolate between the input images.
+  VB : array-like
+    The backward motion field (must have the same shape as I1 and I2). If not 
+    given, VB is assumed to be -VF.
   
   Returns
   -------
@@ -29,8 +32,12 @@ def interpolate(I1, I2, V, n):
     raise ValueError("I1 and I2 must be two-dimensional arrays")
   if I1.shape != I2.shape:
     raise ValueError("I1 and I2 must have the same shape")
-  if V.shape[0:2] != I1.shape or V.shape[0:2] != I2.shape:
+  if VF.shape[0:2] != I1.shape or VF.shape[0:2] != I2.shape or \
+     (VB != None and (VB.shape[0:2] != I1.shape or VB.shape[0:2] != I2.shape)):
     raise ValueError("V must have the same shape as I1 and I2")
+  
+  if VB == None:
+    VB = -VF
   
   X,Y = meshgrid(arange(size(I1, 1)), arange(size(I1, 0)))
   XY = dstack([X, Y])
@@ -39,10 +46,10 @@ def interpolate(I1, I2, V, n):
   
   I_result = []
   for tw in tws:
-    XYW = XY - tw*V[:,:,0:2]
+    XYW = XY + tw*VB[:,:,0:2]
     XYW = [XYW[:, :, 1], XYW[:, :, 0]]
     I1_warped = reshape(map_coordinates(I1, XYW), I1.shape)
-    XYW = XY + (1.0-tw)*V[:,:,0:2]
+    XYW = XY + (1.0-tw)*VF[:,:,0:2]
     XYW = [XYW[:, :, 1], XYW[:, :, 0]]
     I2_warped = reshape(map_coordinates(I2, XYW), I2.shape)
     
