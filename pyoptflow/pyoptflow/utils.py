6bc5d633c6eb44160c2f13f9234c01843133c401
Seppo Pulkkinen <pulkkins@gmail.com>
2018-01-31 17:05:21 +0200

    Additional threshold options and several small corrections.

--- pyoptflow/pyoptflow/utils.py
+++ pyoptflow/pyoptflow/utils.py
@@ -2,11 +2,10 @@
 
 from numpy import isfinite, log, ubyte
 from scipy.ndimage import gaussian_filter
-#from skimage.exposure import equalize_hist, rescale_intensity
 
-def dBZ_to_ubyte(I, dBZ_min=-10.0, dBZ_max=50.0, filter_stddev=3.0):
-  """Convert a dBZ field into a 8-bit image, as required by Optflow. Optionally, 
-  apply a Gaussian smoothing filter.
+def dBZ_to_ubyte(I, dBZ_min=-10.0, dBZ_max=50.0, dBZ_rmin=None, filter_stddev=0.0):
+  """Rescale and convert a reflectivity field in dBZ units into a 8-bit image, 
+  as required by Optflow. Optionally, apply a Gaussian smoothing filter.
   
   Parameters
   ----------
@@ -18,14 +17,20 @@ def dBZ_to_ubyte(I, dBZ_min=-10.0, dBZ_max=50.0, filter_stddev=3.0):
   dBZ_max : float
     Maximum dBZ. Values greater than dBZ_max are set to dBZ_max. If None, 
     dBZ_max is computed from I.
+  dBZ_rmin : float
+    If given, reflectivity values below dBZ_min are set to dBZ_rmin. Defaults to 
+    dBZ_min.
   filter_stddev : float
     Standard deviation of the Gaussian filter (0=no filtering)
   
   Returns
   -------
   out : ndarray(dtype=ubyte)
-    The processed dBZ field.
+    The converted reflectivity field scaled to the range [dBZ_rmin,dBZ_max].
   """
+  if dBZ_max <= dBZ_min:
+    raise ValueError("dBZ_max must be greater than dBZ_min")
+  
   I = I.copy()
   MASK = isfinite(I)
   
@@ -34,31 +39,40 @@ def dBZ_to_ubyte(I, dBZ_min=-10.0, dBZ_max=50.0, filter_stddev=3.0):
   if dBZ_max == None:
     dBZ_max = max(I[MASK])
   
-  I[~MASK] = dBZ_min
-  I[I < dBZ_min] = dBZ_min
+  if dBZ_rmin == None:
+    dBZ_rmin = dBZ_min
+  elif dBZ_rmin > dBZ_min:
+    raise ValueError("dBZ_rmin must not be greater than dBZ_min")
+  
+  I[~MASK] = dBZ_rmin
+  I[I < dBZ_min] = dBZ_rmin
   I[I > dBZ_max] = dBZ_max
   
   if filter_stddev > 0.0:
     I = gaussian_filter(I, filter_stddev, mode="reflect")
   
-  I = ((I - dBZ_min) / (dBZ_max - dBZ_min)) * 255.0
+  I = (I - dBZ_rmin) / (dBZ_max - dBZ_rmin) * 255.0
   
   return I.astype(ubyte)
 
-def rainfall_to_ubyte(I, R_min=0.1, R_max=40.0, filter_stddev=3.0, logtrans=False):
-  """Convert a rainfall intensity field into a 8-bit image, as required by 
-  Optflow. Optionally, apply a Gaussian smoothing filter.
+def rainfall_to_ubyte(R, R_min=0.1, R_max=40.0, R_rmin=None, filter_stddev=0.0, 
+                      logtrans=False):
+  """Rescale and convert a rainfall intensity field into a 8-bit image, as 
+  required by Optflow. Optionally, apply a Gaussian smoothing filter.
   
   Parameters
   ----------
-  I : array-like
+  R : array-like
     The input rainfall field.
   R_min : float
     Minimum rainfall intensity. Values smaller than R_min are set to R_min. 
-    If None, R_min is computed from I.
+    If None, R_min is computed from R.
   R_max : float
     Maximum rainfall intensity. Values greater than R_max are set to R_max. 
-    If None, R_max is computed from I.
+    If None, R_max is computed from R.
+  R_rmin : float
+    If given, rainfall intensity values below R_min are set to R_rmin. Defaults 
+    to R_min.
   filter_stddev : float
     Standard deviation of the Gaussian filter (0=no filtering)
   logtrans : bool
@@ -68,40 +82,40 @@ def rainfall_to_ubyte(I, R_min=0.1, R_max=40.0, filter_stddev=3.0, logtrans=Fals
   Returns
   -------
   out : ndarray(dtype=ubyte)
-    The processed rainfall field.
+    The converted rainfall field scaled to the range [R_rmin,R_max].
   """
-  I = I.copy()
-  MASK = isfinite(I)
+  if R_max <= R_min:
+    raise ValueError("R_max must be greater than R_min")
+  
+  R = R.copy()
+  MASK = isfinite(R)
   
   if R_min == None:
-    R_min = min(I[MASK])
+    R_min = min(R[MASK])
   if R_max == None:
-    R_max = max(I[MASK])
+    R_max = max(R[MASK])
   
-  I[~MASK] = R_min
-  I[I < R_min] = R_min
-  I[I > R_max] = R_max
+  if R_rmin == None:
+    R_rmin = R_min - (R_max - R_min)
+  elif R_rmin > R_min:
+    raise ValueError("R_rmin must not be greater than R_min")
+  
+  R[~MASK] = R_rmin
+  R[R < R_min] = R_rmin
+  R[R > R_max] = R_max
   
   if logtrans == True:
-    if R_min == 0.0:
-      raise ValueError("R_min must be nonzero if log-transform is used")
-    I = log(I)
+    if R_min <= 0.0:
+      raise ValueError("R_min must be positive if log-transform is used")
+    R = log(R)
     R_min = log(R_min)
     R_max = log(R_max)
   
-  # TESTING
-  #I = rescale_intensity(I, (R_min, R_max), (0.0, 1.0))
-  #I = equalize_hist(I)
-  #I = ((I - min(I)) / (max(I) - min(I))) * 255.0
+  R = (R - R_rmin) / (R_max - R_rmin) * 255.0
   
-  MASK = I > R_min
-  # TODO: Make the threshold 128 configurable.
-  I[MASK] = 128.0 + ((I[MASK] - R_min) / (R_max - R_min)) * (255.0 - 128.0)
-  I[~MASK] = 0.0
-  
-  I = I.astype(ubyte)
+  R = R.astype(ubyte)
   
   if filter_stddev > 0.0:
-    I = gaussian_filter(I, filter_stddev, mode="reflect")
+    R = gaussian_filter(R, filter_stddev, mode="reflect")
   
-  return I
+  return R
